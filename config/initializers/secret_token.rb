51bdf3bd72ed2b7bd99013e3010b1757a0d36253
jason speer <speer31@gmail.com>
2013-03-17 17:04:35 -0500

    Finish user edit, update, index, and destroy actions

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '01c86dcc5c652d7fff11d11d3d3b3bb5f611f4bd263daac8231abda08ee658e1805d0fb1e7220200e3d8f8ab3509b68af80a8a58f75433b323e6f0e1dc04538d'
+SampleApp::Application.config.secret_token = 'ced345e01611593c1b783bae98e4e56dbaee787747e92a141565f7c61d0ab2c6f98f7396fb4b178258301e2713816e158461af58c14b695901692f91e72b6200'
