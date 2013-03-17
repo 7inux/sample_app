8d7fdbdced30c10d33c0e6d9c9fd436311429202
jason speer <speer31@gmail.com>
2013-03-16 22:16:39 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
   end
 
 
-  def create
+ def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
